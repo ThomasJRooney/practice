912baeeda1a5dc1a1e9e8f30d22f6b90740b837f
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2022-08-28 19:02:12 -0500

    Update diagonalSort.py

--- diagonalSort.py
+++ diagonalSort.py
@@ -30,3 +30,18 @@ class Solution:
                     mat[min_row][min_col] = cur
                     mat[r][c] = temp
         return mat
+    
+class Solution:
+    def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        d = defaultdict(list)
+        for i in range(m):
+            for j in range(n):
+                d[i - j].append(mat[i][j])
+        for k in d:
+            d[k].sort(reverse=True)
+        for i in range(m):
+            for j in range(n):
+                mat[i][j] = d[i - j].pop()
+        return mat
+                
