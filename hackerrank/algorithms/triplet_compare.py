14137950021145cefb0c1667649e8bec06e98921
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-02 10:35:26 -0500

    Create triplet_compare.py

---
+++ hackerrank/algorithms/triplet_compare.py
@@ -0,0 +1,38 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    ret = [0, 0]
+    for i in range(len(a)):
+        if a[i] > b[i]:
+            ret[0] += 1
+        elif b[i] > a[i]:
+            ret[1] += 1
+    return ret
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
