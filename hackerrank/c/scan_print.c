31d6e081644f82b56b2be84a345458000bcc3c1f
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-02 11:49:37 -0500

    Add practice for the day

---
+++ hackerrank/c/scan_print.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ch;
+    scanf("%c", &ch);
+    printf("%c\n", ch);
+    char s[100];
+    scanf("%s", s);
+    printf("%s\n", s);
+    scanf("\n");
+    scanf("%[^\n]%*c", s);
+    printf("%s\n", s);
+    return 0;
+}
