058e2852e1fcb2ce125d6a1c6dfe64377263e38f
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-04 20:03:51 -0500

    Update kangaroojumps.py

--- hackerrank/algorithms/kangaroojumps.py
+++ hackerrank/algorithms/kangaroojumps.py
@@ -18,17 +18,13 @@ import sys
 #
 
 def kangaroo(x1, v1, x2, v2):
-    if x1 > x2:
-        if v1 >= v2:
-            return("NO")
-        else:
-            return("YES")
-    elif x2 > x1:
-        if v2 >= v1:
-            return("NO")
-        else:
-            return("YES")
-    return("YES")
+    jump_count = x2-x1
+    for i in range(jump_count):
+        x1 = x1+v1
+        x2 = x2+v2
+        if x1==x2:
+            return 'YES'
+    return "NO"
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
