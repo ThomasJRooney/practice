7d5a3a20381d440d1281bf959b7b34b23ac9a2f7
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-09 14:30:54 -0500

    Create between_two_sets.py

---
+++ hackerrank/algorithms/between_two_sets.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    a.sort()
+    b.sort()
+    count = 0
+    for i in range(a[-1], b[0] + 1):
+        canCount = True
+        for n in a:
+            if i % n != 0:
+                canCount = False
+        for n in b:
+            if n % i != 0:
+                canCount = False
+        if canCount:
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
