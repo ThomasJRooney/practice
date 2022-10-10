53a80d59bbd1537f46b73529cd1e1721a01a3428
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-09 19:27:43 -0500

    Create migratorybirds.py

---
+++ hackerrank/algorithms/migratorybirds.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    tmp = [0] * (max(arr) + 1)
+    for i in range(len(arr)):
+        tmp[arr[i]] += 1
+    m = max(tmp)
+    for i in range(len(tmp)):
+        if tmp[i] == m:
+            return i
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
