af608460f016ebd10683fb292a1cce1ff5cf9d0d
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-12 16:18:37 -0500

    Create counting_valleys.py

---
+++ hackerrank/algorithms/counting_valleys.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    count, level = 0, 0
+    for c in path:
+        if c == "U":
+            if level == -1:
+                count += 1
+            level += 1
+        else:
+            level -= 1
+    return count
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
