a3b05326d47a5f823e6509dee6caf0336d2f41aa
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-12 18:44:24 -0500

    Create weather11.sql

---
+++ hackerrank/sql/weather11.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT City FROM STATION WHERE NOT (City LIKE "A%" OR City LIKE "E%" OR City LIKE "I%" OR City LIKE "O%" OR City LIKE "U%") OR NOT (City LIKE "%A" OR City LIKE "%E" OR City LIKE "%I" OR City LIKE "%O" OR City LIKE "%U");
