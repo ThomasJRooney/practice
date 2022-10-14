874b452135b0de3a80b8b00eb0f4eacd2714b6bb
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-14 14:37:09 -0500

    Create capitalize.py

---
+++ hackerrank/python/capitalize.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    s = s.split(" ")
+    ret = []
+    for name in s:
+        ret.append(name.capitalize())
+    return " ".join(ret)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
