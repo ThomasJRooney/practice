1dfa5e67469b6de519d41d6a519d2e05f07a6778
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-16 14:18:35 -0500

    Create utopian_tree.py

---
+++ hackerrank/algorithms/utopian_tree.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    period, h = 0, 1
+    double = True
+    while n > period:
+        if double:
+            h *= 2
+            double = False
+        else:
+            h += 1
+            double = True
+        period += 1
+    return h
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
