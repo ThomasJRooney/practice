aec731c7e2c845f40c6c0b32bea77ca9a92940cc
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-10-16 14:14:23 -0500

    Create pdfviewer.py

---
+++ hackerrank/algorithms/pdfviewer.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    maxh = 1
+    for c in word:
+        idx = alpha.index(c)
+        maxh = max(maxh, h[idx])
+    return len(word) * maxh
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
