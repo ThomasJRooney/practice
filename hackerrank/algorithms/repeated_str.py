0303f6cd4b823b513abcec454497d7214300f645
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-11-01 20:27:57 -0500

    Create repeated_str.py

---
+++ hackerrank/algorithms/repeated_str.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'repeatedString' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. LONG_INTEGER n
+#
+
+def repeatedString(s, n):
+    v = n // len(s)
+    return(s.count('a')*v + s[:n % len(s)].count('a'))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    n = int(input().strip())
+
+    result = repeatedString(s, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
