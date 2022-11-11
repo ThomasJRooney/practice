ed00b156cb48e99ae95237d400e3f813839f3246
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-11-11 13:00:26 -0600

    Create sherlock_square.py

---
+++ hackerrank/algorithms/sherlock_square.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'squares' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER a
+#  2. INTEGER b
+#
+
+def squares(a, b):
+    ret = 0
+    i = math.floor(math.sqrt(a))
+    sq = i * i
+    while sq <= b:
+        if sq >= a:
+            ret += 1
+        i += 1
+        sq = i * i
+    return ret
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        a = int(first_multiple_input[0])
+
+        b = int(first_multiple_input[1])
+
+        result = squares(a, b)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
