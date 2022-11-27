dcb2658679661beaba25f4c7d525e38439c102e0
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-11-27 17:01:32 -0600

    Create LC67_Add_Binary.py

---
+++ leetcode/LC67_Add_Binary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        diff = abs(len(a) - len(b))
+        a = "0" * diff * (len(a) < len(b)) + a
+        b = "0" * diff * (len(b) < len(a)) + b
+        n = len(a)
+        res = []
+        carry = 0
+        for i in range(n - 1, -1, -1):
+            bit1, bit2 = int(a[i]), int(b[i])
+            currBit = (bit1 + bit2 + carry) % 2
+            carry = (bit1 + bit2 + carry) // 2
+            res.append(str(currBit))
+        if carry:
+            res.append(str(carry))
+        return "".join(res)[::-1]
