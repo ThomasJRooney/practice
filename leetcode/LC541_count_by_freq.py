23a1b704bc71651b76affd532e86eb20e7b35314
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-12-03 12:38:24 -0600

    Create LC541_count_by_freq.py

---
+++ leetcode/LC541_count_by_freq.py
@@ -0,0 +1,12 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        d = {}
+        for char in s:
+            d[char] = d.get(char,0) + 1
+
+        sortedList = sorted(d.items(), key = lambda x:x[1])[::-1]
+        res = ''
+
+        for tupleEle in sortedList:
+            res = res+tupleEle[0]*tupleEle[1]
+        return res
