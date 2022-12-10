8a5189a75e8105190491f251849967627fd333c8
Thomas Rooney <thomasjamesrooney12@gmail.com>
2022-12-10 16:51:52 -0600

    Create LC100_sametree.py

---
+++ leetcode/LC100_sametree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not q or not p:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(q.left, p.left) and self.isSameTree(q.right, p.right)
