ceeb3b6f571dbea34bfd376d4b59aeb511a7b128
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2022-12-27 12:28:43 -0800

    Update LC2279_capacity_bags.py

--- leetcode/LC2279_capacity_bags.py
+++ leetcode/LC2279_capacity_bags.py
@@ -12,3 +12,19 @@ class Solution:
             difference[min_index] -= 1
             additionalRocks -= 1
         return difference.count(0)
+
+class Solution:
+    def maximumBags(self, capacity: List[int], rocks: List[int], additionalRocks: int) -> int:
+        l, c = [], 0
+        for i in range(len(capacity)):
+            if capacity[i] - rocks[i]: l.append(capacity[i] - rocks[i])
+            else: c += 1
+        l.sort()
+        for i in range(len(l)):
+            a = l[i]
+            if a <= additionalRocks: 
+                c += 1
+                additionalRocks -= a  
+            else: break
+        return c
+        
