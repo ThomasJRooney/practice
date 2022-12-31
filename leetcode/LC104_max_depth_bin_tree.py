b6a26e2285ea6f893df09878f40e0af70a4c6282
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2022-12-31 13:47:42 -0600

    Create LC104_max_depth_bin_tree.py

---
+++ leetcode/LC104_max_depth_bin_tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        ret = 0
+        if not root: return ret
+        q = [root]
+        while q:
+            for i in range(len(q)):
+                cur = q.pop(0)
+                if cur.left: q.append(cur.left)
+                if cur.right: q.append(cur.right)
+            ret += 1
+        return ret
