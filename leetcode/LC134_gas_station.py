b5628fdb90e3498791d5f84fe8d3e1b6a86b7c00
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-01-07 11:47:21 -0400

    Update LC134_gas_station.py

--- leetcode/LC134_gas_station.py
+++ leetcode/LC134_gas_station.py
@@ -9,3 +9,15 @@ class Solution:
                 surplus = 0
                 start = i + 1
         return -1 if total_surplus < 0 else start
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        start, total_gas, remaining = 0, 0, 0
+        for i in range(n):
+            total_gas += gas[i] - cost[i]
+            remaining += gas[i] - cost[i]
+            if remaining < 0:
+                start = i + 1
+                remaining = 0
+        return start if total_gas >= 0 else -1
