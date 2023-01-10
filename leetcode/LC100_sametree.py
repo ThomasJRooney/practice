aeedd9261cc404dc0b979462a1ccc2a3bba0e286
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-01-10 13:02:26 -0400

    Update LC100_sametree.py

--- leetcode/LC100_sametree.py
+++ leetcode/LC100_sametree.py
@@ -13,3 +13,9 @@ class Solution:
         if p.val != q.val:
             return False
         return self.isSameTree(q.left, p.left) and self.isSameTree(q.right, p.right)
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: return True
+        if not p or not q or p.val != q.val: return False
+        return self.isSameTree(q.left, p.left) and self.isSameTree(q.right, p.right)
