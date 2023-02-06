89faa2a650226c901f12d5b3fc46709752103aa5
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-02-06 14:45:09 -0800

    Create LC278_is_bad_version.py

---
+++ leetcode/LC278_is_bad_version.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low, high = 1, n
+        while low < high:
+            mid = low + (high - low) // 2
+            if isBadVersion(mid): high = mid
+            else: low = mid + 1
+        return low
