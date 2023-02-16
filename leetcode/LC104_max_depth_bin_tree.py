c0548016dde53a88e3ca318a6a2e91d513414f5c
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-02-16 09:52:37 -0800

    Update LC104_max_depth_bin_tree.py

--- leetcode/LC104_max_depth_bin_tree.py
+++ leetcode/LC104_max_depth_bin_tree.py
@@ -16,3 +16,15 @@ class Solution:
                 if cur.right: q.append(cur.right)
             ret += 1
         return ret
+        
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        ret = 0
+        if not root: return ret
+        q = [root]
+        while q:
+            for i in range(len(q)):
+                cur = q.pop(0)
+                if cur.left: q.append(cur.left)
+                if cur.right: q.append(cur.right)
+            ret += 1
+        return ret
