557394f81d67cc7f624f8078adccc14e0dc4a937
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-02-21 06:39:33 -0800

    Create LC540_single_element_in_sorted_arr.py

---
+++ leetcode/LC540_single_element_in_sorted_arr.py
@@ -0,0 +1,8 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        if len(nums) == 1: return nums[0]
+        if nums[0] != nums[1]: return nums[0]
+        if nums[-1] != nums[-2]: return nums[-1]
+        for i in range(1, len(nums) - 1):
+            if nums[i] != nums[i - 1] and nums[i] != nums[i + 1]: return nums[i]
+        return 0
