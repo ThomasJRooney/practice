93f4d1e0f95fe7b99f4c992077938454c12d8057
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-02-28 18:12:22 -0800

    Create LC918.py

---
+++ leetcode/LC918.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def heapify(nums, n, i):
+            l, r = 2 * i + 1, 2 * i + 2
+            largest = i
+            if l < n and nums[largest] < nums[l]: largest = l
+            if r < n and nums[largest] < nums[r]: largest = r
+            if largest != i: 
+                nums[i], nums[largest] = nums[largest], nums[i]
+                heapify(nums, n, largest)
+        n = len(nums)
+        for i in range(n // 2 + 1)[::-1]: heapify(nums, n, i)
+        for i in range(n)[::-1]:
+            nums[i], nums[0] = nums[0], nums[i]
+            heapify(nums, i, 0)
+        return nums
