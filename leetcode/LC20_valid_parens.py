5330e40d39949badbebdf3008db692b9d939cdf0
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-04-10 13:39:47 -0700

    Add valid parens

---
+++ leetcode/LC20_valid_parens.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        match = {"(": ")", "[": "]", "{": "}"}
+        stack = []
+        for char in s:
+            if char in match: stack.append(char)
+            else:
+                if not stack: return False
+                if match[stack[-1]] == char: stack.pop()
+                else: return False
+        return not stack
