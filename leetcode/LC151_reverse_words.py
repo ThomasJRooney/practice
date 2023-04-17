9a653be99c5c84ffbedf765d34bd262ab671ba2d
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-04-16 20:04:47 -0700

    add new solution variant

--- leetcode/LC151_reverse_words.py
+++ leetcode/LC151_reverse_words.py
@@ -1,3 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join([ch for ch in reversed(s.split()) if ch])
+
+class Solution:
+    def reverseWords(self, s: str) -> str: 
+        return " ".join([w for w in reversed(s.split()) if w])
