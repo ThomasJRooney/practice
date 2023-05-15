963846f76151bf624f212434b9561be42f577604
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-05-15 15:32:30 -0700

    add another solution

--- leetcode/LC201_bitwise_and_range.py
+++ leetcode/LC201_bitwise_and_range.py
@@ -6,3 +6,12 @@ class Solution:
             right >>= 1
             i += 1
         return right << i
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        shift = 0
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shift += 1
+        return left << shift
