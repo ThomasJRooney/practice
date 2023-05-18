68b5204be57c90255f05d79acf7cc004366ea345
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-05-18 15:31:41 -0700

    Add indegree

---
+++ leetcode/LC1557_indegree.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        ans, indegree = [], [0] * n
+        for edge in edges: indegree[edge[1]] += 1
+        for i in range(n):
+            if indegree[i] == 0: ans.append(i)
+        return ans
