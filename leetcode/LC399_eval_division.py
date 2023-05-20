45f4f329c0cf4b2a4e421ea0d6cd446ce9c9b93e
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-05-19 18:17:34 -0700

    Add eval division

---
+++ leetcode/LC399_eval_division.py
@@ -0,0 +1,29 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def calcEquation(self, equations, values, queries):
+        graph, results = self.buildGraph(equations, values), []
+        for dividend, divisor in queries:
+            if dividend not in graph or divisor not in graph: results.append(-1.0)
+            else:
+                result = self.bfs(dividend, divisor, graph)
+                results.append(result)
+        return results
+
+    def buildGraph(self, equations, values):
+        graph = defaultdict(dict)
+        for (dividend, divisor), value in zip(equations, values):
+            graph[dividend][divisor] = value
+            graph[divisor][dividend] = 1.0 / value
+        return graph
+    
+    def bfs(self, start, end, graph):
+        queue, visited = deque([(start, 1.0)]), set()
+        while queue:
+            node, value = queue.popleft()
+            if node == end: return value
+            visited.add(node)
+            for neighbor, weight in graph[node].items():
+                if neighbor not in visited: queue.append((neighbor, value * weight))
+        return -1.0
+
