cfc6ccac2d63daa625587618aca98cb524145f6b
Thomas Rooney <44486806+ThomasJRooney@users.noreply.github.com>
2023-06-20 16:47:08 -0700

    Another rep here

--- leetcode/LC1406_stone_game_3.py
+++ leetcode/LC1406_stone_game_3.py
@@ -14,3 +14,19 @@ class Solution:
         if alice > 0: return "Alice"
         elif alice < 0: return "Bob"
         else: return "Tie"
+
+class Solution: 
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        n = len(stoneValue)
+        dp, i = [0] * (n + 1), n - 1
+        while i >= 0:
+            ans = -1001
+            ans = max(ans, stoneValue[i] - dp[i + 1])
+            if i + 1 < n: ans = max(ans, stoneValue[i] + stoneValue[i + 1] - dp[i + 2])
+            if i + 2 < n: ans = max(ans, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3])
+            dp[i] = ans
+            i -= 1
+        alice = dp[0]
+        if alice > 0: return "Alice"
+        elif alice < 0: return "Bob"
+        else: return "Tie"
