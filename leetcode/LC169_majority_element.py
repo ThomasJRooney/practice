be46392c5af1d512bc89925c6a24de07e4705cba
thomasofworld <44486806+ThomasJRooney@users.noreply.github.com>
2024-02-12 14:38:00 -0800

    New solution LC169_majority_element.py

--- leetcode/LC169_majority_element.py
+++ leetcode/LC169_majority_element.py
@@ -1,3 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         return Counter(nums).most_common()[0][0]
+
+class Solution(object):
+    def majorityElement(self, nums):
+        counter_dic = {}
+        for n in nums:
+            if n in counter_dic:
+                counter_dic[n] += 1
+            else:
+                counter_dic[n] = 1
+        return max(counter_dic, key=counter_dic.get)
