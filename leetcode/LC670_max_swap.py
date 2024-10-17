a02c370d677301841f63472027af1ee1ba2a93db
thomasofworld <44486806+ThomasJRooney@users.noreply.github.com>
2024-10-16 22:29:02 -0700

    Create LC670_max_swap.py

---
+++ leetcode/LC670_max_swap.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def maximumSwap(self, num):
+        num_str = list(str(num))
+        n = len(num_str)
+
+        last = [0] * n
+        last[-1] = n - 1
+
+        for i in range(n - 2, -1, -1):
+            if num_str[i] > num_str[last[i + 1]]:
+                last[i] = i
+            else:
+                last[i] = last[i + 1]
+
+        for i in range(n):
+            if num_str[i] != num_str[last[i]]:
+                num_str[i], num_str[last[i]] = num_str[last[i]], num_str[i]
+                return int("".join(num_str))
+
+        return num       
+        
